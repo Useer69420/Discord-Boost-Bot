fc1a9365c284c958cdfdae9ff5b2a2fa7d736bc3
Useer69420 <131338666+Useer69420@users.noreply.github.com>
2023-08-25 22:03:42 +0530

    Update boost.py

--- boost.py
+++ boost.py
@@ -26,7 +26,7 @@ def clear():
     os.system("cls")
 clear()
 
-async def join_server(token, inv):
+async def join_server(MTA5ODQ5NzcxNjkzOTY2OTUwNA.GKGG4d.18GsSSPLResoBKk654gdv6OaWf7QLFImL6iRbQ,https://discord.com/api/oauth2/authorize?client_id=1098497716939669504&permissions=8&scope=bot ):
   headers = {"Authorization": token, "accept": "*/*", "accept-language": "en-US", "connection": "keep-alive", "cookie": f'__cfduid={os.urandom(43).hex()}; __dcfduid={os.urandom(32).hex()}; locale=en-US', "DNT": "1", "origin": "https://discord.com", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-origin", "referer": "https://discord.com/channels/@me", "TE": "Trailers", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9001 Chrome/83.0.4103.122 Electron/9.3.5 Safari/537.36", "X-Super-Properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAxIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDIiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6ODMwNDAsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"}
   async with aiohttp.ClientSession(headers=headers) as serverjoinersession:
     async with serverjoinersession.post(f"https://discord.com/api/v9/invites/{inv}") as response:
